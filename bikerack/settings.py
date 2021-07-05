e9ff1c89dccd08c21267043526c4b92d0144e64d
waldbikerack <waldbikerack@gmail.com>
2021-07-05 13:30:42 +0300

    tweaked app for heroku

--- bikerack/settings.py
+++ bikerack/settings.py
@@ -1,4 +1,7 @@
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -36,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bikerack.urls'
@@ -112,6 +116,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),    
 ]
 
+STATCFILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # myaccount.google.com/lesssecureapps
 # accounts.google.com/DisplayUnlockCaptcha
@@ -125,4 +130,4 @@ EMAIL_HOST_PASSWORD = 'adwpnchlctwiaiwu'
 EMAIL_USE_TLS = True
 # EMAIL_USE_SSL = False 
 
-
+django_heroku.settings(locals())
